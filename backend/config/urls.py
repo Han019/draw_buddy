22e855706b1bf7b3bd4747a5611f686a8121d82a
hsh019 <hsh019@hanmail.net>
2026-06-03 18:47:35 +0900

    admin 수정

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,7 +23,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include("game.urls")),
     path('api/schema/',SpectacularAPIView.as_view(), name="schema"),
     #Swagger UI
